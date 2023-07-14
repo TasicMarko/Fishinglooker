207df1407859d7ef18fa168a7d4cce8c8e8c7836
TasicMarko <tasic.marko.1988@gmail.com>
2023-07-14 15:35:50 +0200

    Single post

--- single.php
+++ single.php
@@ -8,8 +8,36 @@
  */
 
 get_header();
+$backgroundImg = get_the_post_thumbnail_url($post->ID, 'full');
 ?>
 
+<div class="main">
+
+<!-- Hero section -->
+<section class="hero-single"  style="background: url(<?php echo $backgroundImg; ?>)no-repeat center;background-size:cover;">
+    <div class="single-back-overlay"></div>
+    <div class="container">
+        <div class="row">
+            <div class="hero-single-wrap">
+                <h1><?php the_title(); ?></h1>
+                <div class="author-div">
+                <p class="autor-date"><?php echo get_the_author(); ?></p><p class="autor-date"><?php the_date('F j,Y') ?></p>
+                </div>
+                <div class="social-div">
+                    <p class="share">Share This Post</p>
+                    <div class="social-img-box">
+                        <span class="social-span"><i class="fa fa-facebook-official"></i></span>
+                        <span class="social-span"><i class="fa fa-linkedin-square"></i></span>
+                        <span class="social-span"><i class="fa fa-twitter"></i></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Hero section END -->
+
+</div>
 
 <?php
 get_footer();
